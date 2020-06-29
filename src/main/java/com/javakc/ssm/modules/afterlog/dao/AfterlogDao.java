e27863714babc55b6e05ebc04acb381c7815c102
javakc1 <1500401373@qq.com>
2020-06-29 19:30:00 +0800

    W提交第3次，售后日志模块单表查询功能测试成功

---
+++ src/main/java/com/javakc/ssm/modules/afterlog/dao/AfterlogDao.java
@@ -0,0 +1,21 @@
+package com.javakc.ssm.modules.afterlog.dao;
+
+
+import com.javakc.ssm.base.dao.BaseDao;
+import com.javakc.ssm.base.dao.MyBatisDao;
+import com.javakc.ssm.modules.afterlog.entity.AfterlogEntity;
+
+import java.util.List;
+
+
+/**
+ * 售后日志数据层
+ */
+@MyBatisDao
+public interface AfterlogDao extends BaseDao<AfterlogEntity> {
+
+//    public int insert(AfterlogEntity entity);
+
+    public List<AfterlogEntity> findyanyun();
+
+}
--- src/main/java/com/javakc/ssm/modules/afterlog/entity/AfterlogEntity.java
+++ src/main/java/com/javakc/ssm/modules/afterlog/entity/AfterlogEntity.java
@@ -1,18 +1,23 @@
 package com.javakc.ssm.modules.afterlog.entity;
 
 
+import com.javakc.ssm.base.entity.BaseEntity;
+import com.javakc.ssm.modules.organization.entity.OrganizationEntity;
+
 import java.util.Date;
 
 /**
  * 售后日志实体类
  */
-public class AfterlogEntity {
+public class AfterlogEntity extends BaseEntity<AfterlogEntity> {
       private String afterTitle;
       private String afterType;
       private String afterContent;
       private String processingStatus;
       private Date releaseDate;
       private Date closedDate;
+//      OrganizationEntity organizationEntity;
+//      EmployeeEntity employeeEntity;
 
      public String getAfterTitle() {
           return afterTitle;
