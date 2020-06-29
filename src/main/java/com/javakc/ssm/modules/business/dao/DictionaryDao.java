99a3083ed7c22e3861ff036d0e7d59063d019bf7
liuzhaoce <1481878857@qq.com>
2020-06-29 12:33:37 +0800

    0.0.5

---
+++ src/main/java/com/javakc/ssm/modules/business/dao/DictionaryDao.java
@@ -0,0 +1,15 @@
+package com.javakc.ssm.modules.business.dao;
+
+import com.javakc.ssm.base.dao.BaseDao;
+import com.javakc.ssm.base.dao.MyBatisDao;
+import com.javakc.ssm.modules.work.entity.DictionaryEntity;
+
+/**
+ * 数据字典模块数据层实现
+ * @author javakc
+ * @version 0.1
+ */
+@MyBatisDao
+public interface DictionaryDao extends BaseDao<DictionaryEntity>{
+
+}
