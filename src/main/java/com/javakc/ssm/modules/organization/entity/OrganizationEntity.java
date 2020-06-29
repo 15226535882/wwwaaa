1548e51d93df4659a90868f7aa0fe3953c448877
liuzhaoce <1481878857@qq.com>
2020-06-29 17:10:20 +0800

    0.0.7 初步实现多表查询

--- src/main/java/com/javakc/ssm/modules/organization/entity/OrganizationEntity.java
+++ src/main/java/com/javakc/ssm/modules/organization/entity/OrganizationEntity.java
@@ -1,7 +1,10 @@
 package com.javakc.ssm.modules.organization.entity;
 
 import com.javakc.ssm.base.entity.BaseEntity;
+import com.javakc.ssm.modules.business.entity.BusinessEntity;
 import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.List;
 
 /**
  * 数据字典模块实体类
@@ -10,10 +13,11 @@ import org.springframework.format.annotation.DateTimeFormat;
  */
 public class OrganizationEntity extends BaseEntity<OrganizationEntity>{
     private  int id;
+    private List<BusinessEntity> businessEntity;
     /** 机构名称 */
     private String organizationName;
     /** 机构地址 */
-    private String organizationAddress ;
+    private String organizationAddress;
     /** 手机 */
     private int phone ;
     /** 电话 */
@@ -39,5 +43,134 @@ public class OrganizationEntity extends BaseEntity<OrganizationEntity>{
     /** 区域等级 */
     private int areaLevel ;
 
+    public int getId() {
+        return id;
+    }
 
+    public List<BusinessEntity> getBusinessEntity() {
+        return businessEntity;
+    }
+
+    public void setBusinessEntity(List<BusinessEntity> businessEntity) {
+        this.businessEntity = businessEntity;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+
+    public String getOrganizationAddress() {
+        return organizationAddress;
+    }
+
+    public void setOrganizationAddress(String organizationAddress) {
+        this.organizationAddress = organizationAddress;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public int getCountry() {
+        return country;
+    }
+
+    public void setCountry(int country) {
+        this.country = country;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public int getOrganizationLevel() {
+        return organizationLevel;
+    }
+
+    public void setOrganizationLevel(int organizationLevel) {
+        this.organizationLevel = organizationLevel;
+    }
+
+    public int getIndustryCategory() {
+        return industryCategory;
+    }
+
+    public void setIndustryCategory(int industryCategory) {
+        this.industryCategory = industryCategory;
+    }
+
+    public int getImportanceLevel() {
+        return importanceLevel;
+    }
+
+    public void setImportanceLevel(int importanceLevel) {
+        this.importanceLevel = importanceLevel;
+    }
+
+    public int getCompanyLevel() {
+        return companyLevel;
+    }
+
+    public void setCompanyLevel(int companyLevel) {
+        this.companyLevel = companyLevel;
+    }
+
+    public int getAreaLevel() {
+        return areaLevel;
+    }
+
+    public void setAreaLevel(int areaLevel) {
+        this.areaLevel = areaLevel;
+    }
 }
